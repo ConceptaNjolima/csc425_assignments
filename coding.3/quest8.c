3078fb2bdaaced5e1752c98373efb5028c2d06b2
njolimac <njolimac@csc4252021.ssdt.berea.edu>
2021-09-03 12:51:10 -0400

    Edit Quest8 and clean up code

--- coding.3/quest8.c
+++ coding.3/quest8.c
@@ -9,11 +9,13 @@
 int main(int argc, char*argv[]){
 //int file_descriptor=open("Hello.txt", O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
 int pipefds[2];
-char buffer[5];
+char buffer[1025];
 //int wstatus;
+int rc1=fork();
 int rc=fork();
+char *greeting="Hi\0"; // Adding a null termination to end of string
 
-if (rc<0){
+if (rc<0 || rc1<0){
 fprintf(stderr, "fork failed\n");
 exit(1);
 }
@@ -22,33 +24,31 @@ perror("failed to create pipe");
 exit(-1);
 }
 else if (rc==0){
-char greeting[1][20]={"Hi"}; // Adding a null termination to end of string
 close(pipefds[0]); //close the read file descriptor
 
 printf("Child1 writing \n");
-write(pipefds[1],greeting,20);
-//sleep(2);
-printf("done writing");
-sleep(5); // wait for the writing to complete. Without this, the parent was reading before the writing was complete 
+write(pipefds[1],greeting,strlen(greeting));
+//printf("done writing");
 
-//exit(1);
+exit(0);
 //printf("Child %d \n",file_descriptor);
 }
-int rc1=fork();
-if (rc1==0){
-//int wc=waitpid(rc, &wstatus, WUNTRACED | WCONTINUED );// wait for process 0 to complete before process 1 runs
+else if (rc1==0){
+wait(NULL);
 close(pipefds[1]); // close the write file descriptor to ensure reading happens after writing
-//if (wc==-1){
-perror("waitpid");
-exit(-1);
-//}
-//else{
+
 printf("Child2 reading \n");
-read(pipefds[0],buffer,20);
+int n=read(pipefds[0],buffer,sizeof(buffer));
 close(pipefds[0]); // close read file descriptor
-printf("Child2 read:'%s' \n",buffer); // read from the buffered greeting
+printf("Child2 read:'%s' %d \n ",buffer,n); // read from the buffered greeting
+
 exit(1);
-//}
+
+}
+else{
+close(pipefds[0]);
+close(pipefds[1]); // Parent process closes all file descriptors
+exit(0);
 }
 return 0;
 
