ccd6918ed2488ed85750ee3d691a8f3de64f6729
njolimac <njolimac@csc4252021.ssdt.berea.edu>
2021-09-20 16:54:52 -0400

    Add running with large amounts of memory in Question 4 and delete the .save file

--- coding.7/memory-user.c
+++ coding.7/memory-user.c
@@ -13,7 +13,7 @@ printf("Received one argument! Expected the number of megabytes as the second ar
 }
 
 //print the process's id
-printf("The process id is %d",getpid());
+printf("The process id is %d \n",getpid());
 
 int mem_requested=atoi(argv[1]);
 printf("saved %d \n",mem_requested);
@@ -24,14 +24,18 @@ int array_size=0;
 array_size=mem_requested/sizeof(int);
 printf("array_size %d\n",array_size);
 
-int my_array[array_size];
+int *my_array;
+//using calloc which is the malloc version for array
+my_array=calloc(array_size,sizeof(int));
 //loop through indefinately
 
 while (1){
-for (int i=0;i<sizeof(my_array);i++){
-my_array[i]=my_array[i]+1;
+for (int i=0;i<array_size;i++){
+//put the value of i in array element i
+*(my_array+1)=i;
+//printf("%d \n",*my_array+i);
 }
 }
-
+free(my_array);
 return 0;
 }
